63670c5c25915b21d9f8209535d167d2b7c7953e
Mythricia <mythricia@gmail.com>
2018-11-28 21:35:45 +0100

    Removed unused chain scripts

--- Assets/Scripts/ChainLink.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ChainLink : MonoBehaviour {
-	public GameObject nextLink;
-	public GameObject prevLink;
-}
